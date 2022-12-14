220cd34fc4e304cb45aa5371d98aefee321dd1dc
이재혁 <80149657+LeeJeaHyuk@users.noreply.github.com>
2022-12-14 16:32:57 +0900

    file update

---
+++ 난이도0/001옹알이.py
@@ -0,0 +1,20 @@
+input1 = ["aya", "yee", "u", "maa", "wyeoo"]
+
+input2 = ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]
+
+
+# def solution(babbling):
+    
+#     for i in range(len(babbling)):
+#         if babbling[i][:3] == 'aya':            
+#             print(check(i,babbling[i]))
+            
+#     answer = 0
+#     return answer
+
+# def check(i,aya):
+#     if aya == 'aya':
+#         print(aya)
+#         return aya[-3:]
+
+# solution(input1)
